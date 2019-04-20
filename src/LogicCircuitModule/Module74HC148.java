a02f9b710d464361c5598a2495c2a7e02bad047e
WangTingZheng <2251985371@qq.com>
2019-04-20 23:42:17 +0800

    v5.4
    74HC148完整更新
    已测试

--- src/LogicCircuitModule/Module74HC148.java
+++ src/LogicCircuitModule/Module74HC148.java
@@ -1,7 +1,9 @@
 package LogicCircuitModule;
 import DescriptionForLogicFunction.OperationSymbol;
+
 public class Module74HC148 {
-	static int input[]= {0,0,1,1,1,1,1,1,1};
+	static int input[]= {0,0,0,0,0,0,0,0,1};// 0-7 I0µ½I7£¬8£ºS
+	static int output[]= {0,0,0};
 	
 	static class instructions{
 		public static int YexNor(int inputtemp[]) {
@@ -20,18 +22,61 @@ public class Module74HC148 {
 	}
 	
 	static class outPut{
-		public static int Y0Nor(int inputtemp[]) {//
+		public static int Y0Nor(int inputtemp[]) {
+			int outPutTemp;
 			OperationSymbol  os=new OperationSymbol();
-			int outPut=0;
-			//outPut=osBase.nor()
-			return outPut;
+			int list[][]= {{inputtemp[1],os.not(inputtemp[2]),os.not(inputtemp[4]),os.not(inputtemp[6])},
+						   {inputtemp[3],os.not(inputtemp[4]),os.not(inputtemp[6])},
+					       {inputtemp[6],os.not(inputtemp[6])},
+					       {inputtemp[7]}};
+			outPutTemp=os.many(list[0], "and");
+			for(int i=1;i<4;i++) {
+				outPutTemp = os.or(os.many(list[0],"and"),outPutTemp);
+			}
+			return os.and(outPutTemp,inputtemp[0]);
+		}
+			
+		public static int Y1Nor(int inputtemp[]) {
+			int outPutTemp;
+			OperationSymbol os=new OperationSymbol();
+			int list[][]= {{inputtemp[2],os.not(inputtemp[4]),os.not(inputtemp[5])},{inputtemp[3],os.not(inputtemp[4]),os.not(inputtemp[5])},
+						   {inputtemp[6]},{inputtemp[7]}};
+			outPutTemp=os.many(list[0],"and");
+			for(int i=1;i<4;i++) {
+				outPutTemp = os.or(os.many(list[0],"and"),outPutTemp);
+			}
+			return os.and(outPutTemp,inputtemp[0]);
+			
+		}
+		public static int Y2Nor(int inputtemp[]) {//
+			int outPutTemp;
+			OperationSymbol os=new OperationSymbol();
+			int tmp[]=new int [4];
+			for(int i=0;i<4;i++) {
+				tmp[i]=inputtemp[i+4];
+			}
+			outPutTemp=os.many(tmp,"or");
+			return os.and(outPutTemp,inputtemp[0]);
 		}
 	}
 	
-
 	public static void main(String args[]) {
 		instructions i=new instructions();
-		System.out.println(i.YsNor(input));
+		OperationSymbol os=new OperationSymbol();
+		outPut o=new outPut();
+		output[0]=o.Y0Nor(input);
+		output[1]=o.Y1Nor(input);
+		output[2]=o.Y2Nor(input);
+		for(int ix=2;ix>=0;ix--) {
+			System.out.print(os.not(output[ix])+" ");
+		}
+		System.out.println("");
+		for(int ix=0;ix<9;ix++) {
+			System.out.print(os.not(input[ix])+" ");
+		}
+		System.out.println("");
 	}
-
 }
+
+
+
